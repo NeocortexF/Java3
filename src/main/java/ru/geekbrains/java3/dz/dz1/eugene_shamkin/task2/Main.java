b43ab5dc42fe60b49082f06495c2b26bff165568
NeocortexF <eugeneshamkin@mail.ru>
2017-04-11 18:52:08 +0500

    task 2

--- src/main/java/ru/geekbrains/java3/dz/dz1/eugene_shamkin/task2/Main.java
+++ src/main/java/ru/geekbrains/java3/dz/dz1/eugene_shamkin/task2/Main.java
@@ -1,22 +1,27 @@
 package ru.geekbrains.java3.dz.dz1.eugene_shamkin.task2;
 
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 
-/** Geekbrains.ru
- *  Course name: Java 3
- *  Homework 1, performed by Eugene Shamkin, April 11, 2017
- *  **********************************************************
- *  Написать метод, который преобразует массив в ArrayList;
+/**
+ * Geekbrains.ru
+ * Course name: Java 3
+ * Homework 1, performed by Eugene Shamkin, April 11, 2017
+ * **********************************************************
+ * Написать метод, который преобразует массив в ArrayList;
  */
 public class Main {
 
-       public static void main(String[] args) {
-           int[] arr={1,2,3,4,5,6,7,8,9};
-           List<?> list= Arrays.asList(arr);
-           System.out.println(list);
-       }
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        String array[] = {"Строка 1", "Строка 2", "Строка 3", "Строка 4"};
+        for (int i = 0; i < array.length; i++) {
+            arrayList.add(array[i]);
+        }
+        for (String str : arrayList) {
+            System.out.println(str);
+        }
+    }
 }
 
 
