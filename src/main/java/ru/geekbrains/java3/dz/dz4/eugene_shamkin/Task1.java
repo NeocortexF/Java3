7484ee6b25399ed501cae4213c437cff5f5ad791
NeocortexF <eugeneshamkin@mail.ru>
2017-04-25 17:27:08 +0500

    task 1 done

--- src/main/java/ru/geekbrains/java3/dz/dz4/eugene_shamkin/Task1.java
+++ src/main/java/ru/geekbrains/java3/dz/dz4/eugene_shamkin/Task1.java
@@ -1,70 +1,49 @@
 package ru.geekbrains.java3.dz.dz4.eugene_shamkin;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Geekbrains.ru
  * Course name: Java 3
  * Homework 4 performed by Eugene Shamkin, April 25, 2017
  * **********************************************************
- *
+ * <p>
  * Создать три потока, каждый из которых выводит определенную букву(A, B и C) 5 раз,
  * порядок должен быть именно ABСABСABС. Используйте wait/notify/notifyAll.
  */
 public class Task1 {
 
-    private final Object mon = new Object();
-    private volatile char currentLetter = 'A';
-
     public static void main(String[] args) {
+        ExecutorService serv = Executors.newSingleThreadExecutor();
         Task1 w = new Task1();
-        Thread t1 = new Thread(() -> { w.printA();
+        Thread t1 = new Thread(() -> {
+            w.printA();
         });
-        Thread t2 = new Thread(() -> { w.printB();
+        Thread t2 = new Thread(() -> {
+            w.printB();
         });
-        Thread t3 = new Thread(() -> { w.printC();
+        Thread t3 = new Thread(() -> {
+            w.printC();
         });
-        t1.start();
-        t2.start();
-        t3.start();
-    }
-        public void printA() {
-        synchronized (mon) {
-            try {
-                for (int i = 0; i < 3; i++) {
-                    while (currentLetter != 'A') mon.wait();
-                    System.out.print("A");
-                    currentLetter = 'B';
-                    mon.notify();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
+        for (int i = 0; i < 3; i++) {
+            serv.execute(t1);
+            serv.execute(t2);
+            serv.execute(t3);
         }
     }
+
+    public void printA() {
+        System.out.print("A");
+    }
+
     public void printB() {
-        synchronized (mon) {
-            try {
-                for (int i = 0; i < 3; i++) {
-                    while (currentLetter != 'B')
-                        mon.wait(); System.out.print("B");
-                        currentLetter = 'C';
-                        mon.notify();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        System.out.print("B");
     }
+
     public void printC() {
-        synchronized (mon) {
-            try {
-                for (int i = 0; i < 3; i++) {
-                    while (currentLetter != 'C')
-                        mon.wait(); System.out.print("C");
-                    currentLetter = 'A'; mon.notify();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        System.out.print("C");
     }
+
 }
