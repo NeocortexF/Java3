4ea18abcf46237bd37f51582fc6d5e3edd2db949
NeocortexF <eugeneshamkin@mail.ru>
2017-04-27 11:30:21 +0500

    homework 5 preparaation

---
+++ src/main/java/ru/geekbrains/java3/dz/dz5/eugene_shamkin/MainClass.java
@@ -0,0 +1,34 @@
+package ru.geekbrains.java3.dz.dz5.eugene_shamkin;
+
+// Домашнее задание
+// Организуем гонки
+// Есть набор правил:
+// Все участники должны стартовать одновременно, несмотря на то что на подготовку у каждого уходит разное время
+// В туннель не может заехать одновременно больше половину участников(условность)
+// Попробуйте все это синхронизировать
+// Только после того как все завершат гонку нужно выдать объявление об окончании
+// Можете корректировать конструктор машин и добавлять объекты классов из пакета util.concurrent
+// исходники копируем в папку со своей домашкой
+
+import ru.geekbrains.java3.lesson5.dz5.Car;
+import ru.geekbrains.java3.lesson5.dz5.Race;
+import ru.geekbrains.java3.lesson5.dz5.Road;
+import ru.geekbrains.java3.lesson5.dz5.Tunnel;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Подготовка!!!");
+        ru.geekbrains.java3.lesson5.dz5.Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
