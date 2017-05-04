86b39c1f5e8dc2391bc4c5c396225ecdea9ba754
NeocortexF <eugeneshamkin@mail.ru>
2017-05-04 14:44:12 +0500

    tests for task2

---
+++ src/test/java/ru/geekbrains/java3/dz/dz6/eugene_shamkin/Task2Test.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.java3.dz.dz6.eugene_shamkin;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Администратор on 04.05.2017.
+ */
+public class Task2Test {
+    int[] arrayWith1 = new int[] {5, 6, 1, 2, 7};
+    int[] arrayWith4and1 = new int[] {1, 2, 3, 4, 5, 6};
+    int[] arrayWithout4and1 = new int[] {2, 3, 5, 6, 7, 2};
+    Task2 task2 = new Task2();
+
+    @Test
+    public void isArrayContainsOnly4and1() throws Exception {
+        Assert.assertTrue(task2.isArrayContainsOnly4and1(arrayWith4and1));
+    }
+
+    @Test
+    public void isArrayContains1() throws Exception {
+        Assert.assertTrue(task2.isArrayContainsOnly4and1(arrayWith1));
+    }
+
+    @Test
+    public void isArrayWithout4and1() throws Exception {
+        Assert.assertFalse(task2.isArrayContainsOnly4and1(arrayWithout4and1));
+    }
+
+
+}
